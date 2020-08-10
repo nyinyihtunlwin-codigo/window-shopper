2b2c0fd777ce180011c233380c1cbe2654464c28
Nyi Nyi Htun Lwin <nnhl.mgk@gmail.com>
2020-08-10 16:58:30 +0630

    update : added custom text field

--- window-shopper/window-shopper/views/CustomTextField.swift
+++ window-shopper/window-shopper/views/CustomTextField.swift
@@ -13,6 +13,16 @@ class CustomTextField: UITextField {
     override func awakeFromNib() {
         super.awakeFromNib()
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.244140625)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if placeholder == nil {
+            placeholder = " "
+        }
+        
+        let place = NSAttributedString(string: placeholder!, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
+        attributedPlaceholder = place
+        textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
     }
 
 }
