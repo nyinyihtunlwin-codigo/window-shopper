6b3ab124b54e50c98158b9656411a6c120db71c6
Nyi Nyi Htun Lwin <nnhl.mgk@gmail.com>
2020-08-10 16:51:05 +0630

    update : added assets

---
+++ window-shopper/window-shopper/views/CustomTextField.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTextField.swift
+//  window-shopper
+//
+//  Created by Administrator on 8/10/20.
+//  Copyright © 2020 Nyi Nyi Htun Lwin. All rights reserved.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.244140625)
+    }
+
+}
